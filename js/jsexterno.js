0a85fe8ff3d4d70e039b5b94f66e0d9dcb6fa7c7
Zadhield <afpujota@uce.edu.ec>
2025-10-31 19:44:36 -0500

    Taller 5: Principio de JS

---
+++ js/jsexterno.js
@@ -0,0 +1,62 @@
+function cambiarColor(id_elemento, color) {
+    document.getElementById(id_elemento).style.color = color;
+}
+
+function agregarElemento(idElementoPadre, html) {
+    document.getElementById(idElementoPadre).innerHTML = html;
+}
+
+function construirH1() {
+    return '<h1 id="Id_calculadora"> Calculadora</h1>';
+}
+
+function eliminarElemento(idElemento) {
+    document.getElementById(idElemento).remove();
+}
+
+function ocultarElemento(idElemento) {
+    document.getElementById(idElemento).style.display = 'none';
+}
+
+function mostrarElemento(idElemento) {
+    document.getElementById(idElemento).style.display = 'block';
+}
+
+function evaluarOperacion(tipo) {
+    
+    let num1 = parseFloat(document.getElementById('id_n1').value);
+    let num2 = parseFloat(document.getElementById('id_n2').value);
+    
+    let resultado = 0;
+
+    if(tipo === '+') {
+        resultado = sumar(num1, num2);
+    }
+    if(tipo === '-') {
+        resultado = restar(num1, num2);
+    }
+    if(tipo === '*') {
+
+    }
+    if(tipo === '/') {
+
+    }
+    document.getElementById('id_resultado').innerText = resultado;
+
+}
+
+function sumar(num1, num2) {
+    return num1 + num2;
+}
+
+function restar(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+
+function dividir(num1, num2) {
+    return num1 / num2;
+}
